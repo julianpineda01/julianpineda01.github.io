d4cf10e37594199a43d6df711466711228055e7f
julianpineda01 <gaitanlucila203@gmail.com>
2024-10-20 18:58:54 -0500

    Primer Sitio Web

---
+++ php/conexion_be.php
@@ -0,0 +1,14 @@
+<?php
+
+
+    $conexion = mysqli_connect("localhost", "root", "", "inicio_registro_db");
+
+    /*
+    if($conexion){
+        echo 'Conectado exitosamente a la Base de Datos';
+    }else{
+        echo 'No se ha podido conectar a la Base de Datos';
+    }
+    */
+
+?>
